ff97b3eee02eb16dc38fb7771cd2da5694fedb40
Meenal-Poladia <meenal.poladia94@gmail.com>
2021-10-17 17:17:25 +0530

    Added 10-globals.js

---
+++ 10-globals.js
@@ -0,0 +1,16 @@
+/*
+
+    Globals
+
+    These are the config defaults that will be applied to every requests. The developer
+    can access these to view the content according to ones login and role.
+    
+
+
+Example 1:
+axios.defaults.headers.common[Authorization] = "access token"
+
+OR
+
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+ */
