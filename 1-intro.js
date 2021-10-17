e9d5fa286e3d596c71316887916062cc9dc84cb2
Meenal-Poladia <meenal.poladia94@gmail.com>
2021-10-17 17:10:45 +0530

    Added 1-intro.js

---
+++ 1-intro.js
@@ -0,0 +1,14 @@
+/*
+    Introduction
+
+    Axios is a react library which is used to call third party API from the backend
+    or the server.
+    It is similar to the fetch API however the syntax is better. It can be used with
+    vanilla javascript, node js or for vue applications.
+
+    Axios always returns a promise so .then is used to consume it, catch is used for
+    error handling and finally for rendering data inspite of .then or catch. Async and await
+    also can be used.
+
+    Library can be imported from npm to use axios.
+ */
