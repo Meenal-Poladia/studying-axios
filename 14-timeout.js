e0f90f3225d648ed3061a953dbd746bf8f3dd28e
Meenal-Poladia <meenal.poladia94@gmail.com>
2021-10-17 17:19:13 +0530

    Added 14-timeout.js

---
+++ 14-timeout.js
@@ -0,0 +1,16 @@
+/*
+
+    TimeOut
+
+    Setting timeout helps the page to stop loading and dispaly error if it has
+    exceeded the specified time.
+    The time is calculated in milliseconds.
+
+
+Example 1:
+
+axios
+    .get("https://jsonplaceholder.typicode.com/todos", {
+       timeout: 5000
+     })
+ */
